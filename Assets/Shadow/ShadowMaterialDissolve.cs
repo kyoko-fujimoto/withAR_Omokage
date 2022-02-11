57ff90f8328d484683565652eeadc72932823b4d
hidenori <hidenori_i@pretia.co.jp>
2022-02-11 17:13:01 +0900

    -shadow_femaleの実装・dissolveのトリガー関係の入れ替え

---
+++ Assets/Shadow/ShadowMaterialDissolve.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShadowMaterialDissolve : MonoBehaviour
+{
+    [SerializeField] private Material _material;
+
+    public IEnumerator ShadowFade(float duration, float startScale, float endScale)
+    {
+        for (float t = 0; t <= duration; t += Time.deltaTime)
+        {
+            _material.SetFloat("_cavity", Mathf.Lerp(startScale, endScale, t / duration));
+            Debug.Log("dissolve!");
+            yield return null;
+        }
+    }
+}
