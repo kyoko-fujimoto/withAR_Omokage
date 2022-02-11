412bcc0481eac903c942333a72c73af2f4f08aa4
kyoko-fujimoto <russula.kyometica1998@gmail.com>
2022-02-11 17:15:28 +0900

    UI をワイヤーフレームに合わせてアップデート

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -7,46 +7,17 @@ public class ButtonManager : MonoBehaviour
 {
     public static ButtonManager instance;
 
-    [SerializeField] GameObject TopPanel;
-    [SerializeField] GameObject ScanPanel;
-    
-    [SerializeField] GameObject Header;
-    
     public void Awake()
     {
         if(instance == null)
         {
             instance = this;
         }
-
-        SetHeader(false);
-    }
-
-    // ヘッダーを表示するかどうかを設定する
-    public void SetHeader(bool isActived)
-    {
-        Header.SetActive(isActived);
     }
 
     // メインシーンへ移動
     public void LoadMainScene()
     {
         SceneManager.LoadScene("MainScene");
-    }
-
-    // TopCanvas を呼び出し
-    public void OpenTopCanvas()
-    {
-        TopPanel.SetActive(true);
-        ScanPanel.SetActive(false);
-    }
-    
-    // ScanCanvas を呼び出し
-    public void OpenScanCanvas()
-    {
-        TopPanel.SetActive(false);
-        ScanPanel.SetActive(true);
-
-        SetHeader(true);
     }
 }
